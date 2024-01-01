d85f52e9e3adb63c120c4f447a0a7aebc20dd816
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-01 21:00:14 +0530

    initial backend setup

---
+++ backend/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT = 3000 || process.env.PORT;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", (req: Request, res: Response) => {
+  res.json({ message: "hello from test endpoint!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on localhost:${PORT}`);
+});
