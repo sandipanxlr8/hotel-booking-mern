d5cba59db29939d2a96f7b7e9ab4b7c84b184d6e
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-02 23:53:15 +0530

    Add header and hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,14 @@
+function Hero() {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-4xl text-white font-bold ">Find your next stay</h1>
+        <p className="text-xl text-white">
+          Search budget hotels for your dream vacation
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
