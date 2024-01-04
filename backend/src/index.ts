2c6608bf69bf3ac58533be352f50e74fdfba8175
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-04 23:44:44 +0530

    feature: add sign up register route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoutes from "./routes/users";
 
 const PORT = 3000 || process.env.PORT;
 const app = express();
@@ -9,9 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", (req: Request, res: Response) => {
-  res.json({ message: "hello from test endpoint!" });
-});
+app.use("/api/users", userRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URL as string)
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,52 @@
+import express, { Request, Response } from "express";
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import { check, validationResult } from "express-validator";
+
+const router = express.Router();
+
+router.post(
+  "/register",
+  [
+    check("firstName", "First name is requried").isString(),
+    check("lastName", "Last name is requried").isString(),
+    check("email", "Email is requried with correct format").isEmail(),
+    check("password", "Password with 6 or more characters required").isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ message: errors.array() });
+
+    try {
+      let user = await User.findOne({
+        email: req.body.email,
+      });
+
+      if (user) return res.status(400).json({ message: "User alrady exits" });
+
+      user = new User(req.body);
+      await user.save();
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: "2h" }
+      );
+
+      res.cookie("auth_token", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "PRODUCTION",
+        maxAge: 7200000,
+      });
+      return res.sendStatus(200);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "something went wrong" });
+    }
+  }
+);
+
+export default router;
