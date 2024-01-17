8759afea5ae4d91e7d923fce3d62e6aac9e23f39
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-17 23:38:05 +0530

    add hotel form with details, type, facilities, guests section

---
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,35 @@
+import { FormProvider, useForm } from "react-hook-form";
+import DetailsSection from "./DetailsSection";
+import TypeSection from "./TypeSection";
+import FacilitiesSection from "./FacilitiesSection";
+import GuestsSection from "./GuestsSection";
+
+export type HotelFormData = {
+  name: string;
+  city: string;
+  country: string;
+  description: string;
+  type: string;
+  pricePerNight: number;
+  startRating: number;
+  facilities: string[];
+  imageFiles: FileList;
+  adultCount: number;
+  childCount: number;
+};
+
+function ManageHotelForm() {
+  const formMethods = useForm<HotelFormData>();
+  return (
+    <FormProvider {...formMethods}>
+      <form className="flex flex-col gap-10">
+        <DetailsSection />
+        <TypeSection />
+        <FacilitiesSection />
+        <GuestsSection />
+      </form>
+    </FormProvider>
+  );
+}
+
+export default ManageHotelForm;
---
+++ frontend/src/pages/AddHotel.tsx
@@ -0,0 +1,11 @@
+import ManageHotelForm from "@/forms/ManageHotelForm/ManageHotelForm";
+
+function AddHotel() {
+  return (
+    <div>
+      <ManageHotelForm />
+    </div>
+  );
+}
+
+export default AddHotel;
