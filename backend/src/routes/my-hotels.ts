bb3fb44a4ad778599e898773263a20df61c607e5
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-19 01:49:54 +0530

    add view details page

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -64,4 +64,14 @@ router.post(
   }
 );
 
+router.get("/", verifyToken, async (req: Request, res: Response) => {
+  try {
+    console.log("userId : ", req.userId);
+    const hotels = await Hotel.find({ userId: req.userId });
+    res.json(hotels);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching hotels" });
+  }
+});
+
 export default router;
