32e48645986a4fe6aeab49f676dd3bef3efa846f
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-26 22:57:43 +0530

    integrate api for update hotel frontend form and backend endpoint

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -112,6 +112,7 @@ router.put(
       ];
 
       await hotel.save();
+      res.status(201).json(hotel);
     } catch (error) {
       res.status(500).json({ message: "Something went wrong" });
     }
