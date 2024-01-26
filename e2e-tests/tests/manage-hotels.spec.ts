1a15b8c55c177d5b72314f72e8e84da679f490be
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-27 00:03:18 +0530

    add manage hotel e2e test

--- e2e-tests/tests/manage-hotels.spec.ts
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -62,3 +62,23 @@ test("should display hotels", async ({ page }) => {
     page.getByRole("link", { name: "View Details" }).first()
   ).toBeVisible();
 });
+
+test("should edit hotel", async ({ page }) => {
+  await page.goto(`${UI_URL}my-hotels`);
+
+  await page.getByRole("link", { name: "View Details" }).first().click();
+
+  await page.waitForSelector('[name="name"]', { state: "attached" });
+  await expect(page.locator('[name="name"]')).toHaveValue("Indian Getaways");
+  await page.locator('[name="name"]').fill("Indian Getaways UPDATED");
+  await page.getByRole("button", { name: "Save" }).click();
+  await expect(page.getByText("Hotel Saved")).toBeVisible();
+
+  await page.reload();
+  await expect(page.locator('[name="name"]')).toHaveValue(
+    "Indian Getaways UPDATED"
+  );
+
+  await page.locator('[name="name"]').fill("Indian Getaways");
+  await page.getByRole("button", { name: "Save" }).click();
+});
