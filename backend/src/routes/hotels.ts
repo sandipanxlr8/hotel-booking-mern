32e7fb5e3ebced37d8fd0696d7248cf61e37bac2
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-01-29 01:26:25 +0530

    integrate backend api to search for hotels

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -4,7 +4,7 @@ import { HotelSearchResponse } from "../shared/types";
 
 const router = express.Router();
 
-router.get("search", async (req: Request, res: Response) => {
+router.get("/search", async (req: Request, res: Response) => {
   try {
     const pageSize = 5;
     const pageNumber = parseInt(
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,10 +1,26 @@
 import { useSearchContext } from "@/contexts/SearchContext";
+import { useQuery } from "react-query";
+import * as apiClient from "../api-client";
+import { useState } from "react";
 
 function Search() {
   const search = useSearchContext();
-  console.log(search);
+  const [page, setpage] = useState<number>(1);
 
-  return <div>Search</div>;
+  const searchParams = {
+    destination: search.destination,
+    checkIn: search.checkIn.toISOString(),
+    checkOut: search.checkOut.toISOString(),
+    adultCount: search.adultCount.toString(),
+    childCount: search.childCount.toString(),
+    page: page.toString(),
+  };
+
+  const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
+    apiClient.searchHotels(searchParams)
+  );
+
+  return <div>Search {hotelData?.data.length}</div>;
 }
 
 export default Search;
