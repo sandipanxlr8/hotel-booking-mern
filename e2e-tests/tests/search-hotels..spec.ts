c3ff9782cbea765a4c5908e9e8844a0564040162
Sandipan Kalita <sandipankalitaofficial@gmail.com>
2024-02-07 23:42:00 +0530

    add book hotel e2e test

--- e2e-tests/tests/search-hotels..spec.ts
+++ e2e-tests/tests/search-hotels..spec.ts
@@ -36,3 +36,40 @@ test("should show hotel detail", async ({ page }) => {
   await expect(page).toHaveURL(/detail/);
   await expect(page.getByRole("button", { name: "Book Now" })).toBeVisible();
 });
+
+test("should boook hotel", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("Guwahati");
+
+  const date = new Date();
+  date.setDate(date.getDate() + 3);
+  const formattedDate = date.toISOString().split("T")[0];
+  await page.getByPlaceholder("Check-out Date").fill(formattedDate);
+
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Indian Getaways").click();
+  await page.getByRole("button", { name: "Book Now" }).click();
+
+  await expect(page.getByText("Total Cost: ₹3000.00")).toBeVisible();
+
+  const stripeFrame = page.frameLocator("iframe").first();
+  await stripeFrame
+    .locator('[placeholder="Card number"]')
+    .fill("4000003560000008");
+  await stripeFrame.locator('[placeholder="MM / YY"]').fill("04/40");
+  await stripeFrame.locator('[placeholder="CVC"]').fill("123");
+
+  console.log("test log");
+  await page.getByRole("button", { name: "Confirm Booking" }).click();
+
+  const confrimStripeFrame = page.frameLocator("iframe").first();
+  const confrimPaymentStripeFrame = confrimStripeFrame
+    .frameLocator("iframe")
+    .first();
+
+  await confrimPaymentStripeFrame
+    .getByRole("button", { name: "Complete" })
+    .click();
+});
